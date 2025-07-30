b69332e94104b7d0218c44d44a218c06799b5523
bettydots <93158166+bettydots@users.noreply.github.com>
2025-07-30 00:45:42 -0500

    Finished exercise 07
    Turned string into array, initialized empty array, did for loop
    where i is string array length. Used at(i-1) to target the last
    element of string array and push() into empty array. Returned
    reversed array with join() to turn back into string. toString adds
    commas in between and the replaceAll method would cause an issue if
    the given string had commas.

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  let stringArr = string.split("")
+  let reverseArr = []
+  for (let i = stringArr.length; i > 0; i--) {
+    reverseArr.push(stringArr.at(i - 1))
+  }
+  return reverseArr.join("")
 };
+console.log(reverseString("Hello"))
 
 // Do not edit below this line
 module.exports = reverseString;
